0bf65761cca3f80b86c0fa0b03e854fb14a6275a
Hasan Ashab <samertask@gmail.com>
2025-09-10 09:45:18 +0600

    backup

--- vars/docker_build_and_scan.groovy
+++ vars/docker_build_and_scan.groovy
@@ -7,12 +7,12 @@ def call(Map params) {
         docker_build(fullImageBase, params.Tags, params.ImageSuffix)
 
         // Scan the built image with Trivy.
-        // Trivy needs the full image name and the tag.
-        // trivy_scan_docker(
-        //     Image: fullImageBase,
-        //     Tag: params.Tags[0],
-        //     Severity: ['HIGH', 'CRITICAL'],
-        //     Scanners: ['os', 'library'],
-        // )
+        Trivy needs the full image name and the tag.
+        trivy_scan_docker(
+            Image: fullImageBase,
+            Tag: params.Tags[0],
+            Severity: ['HIGH', 'CRITICAL'],
+            Scanners: ['os', 'library'],
+        )
     }
 }
